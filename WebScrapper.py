e3b0bb44468afe7cad155294a0ec202d315f6944
maxxa <hondaunispree@gmail.com>
2021-12-21 22:52:34 -0800

    On branch main  Changes to be committed: 	modified:   WebScrapper.py 	modified:   WebScrapper.sublime-workspace

--- WebScrapper.py
+++ WebScrapper.py
@@ -2,38 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://realpython.github.io/fake-jobs"
+URL = "https://skagit.craigslist.org/d/free-stuff/search/zip"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="ResultsContainer")
+results = soup.find(id="search-results")
 print(results.prettify())
 
-python_job_elements = results.find_all("div", class_="card-content")
+python_job_elements = results.find_all("li", class_="result-row" )
 
 for job_element in python_job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
     links = job_element.find_all("a")
     for link in links:
         link_url = link["href"]
-        print(f"Apply here: {link_url}\n")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()
+        print( link_url)
 
-# python_jobs = results.find_all("h2", string="Python") 
+    # print(job_element.text.strip())
+#    post_links = job_element.find("a", class_="result-title hdrlnk")
+#    company_element = job_element.find("h3", class_="company")
+#     location_element = job_element.find("p", class_="location")
+#links = job_element.find_all("a")
+#for link in links:
+#   for link in links:
+# pply here: {link_url}\n")
+#     print(title_element.text.strip())
+#     print(company_element.text.strip())
+#     print(location_element.text.strip())
+#     print()
 
-# above code has to be modified because string function is case sensitve
+# # python_jobs = results.find_all("h2", string="Python") 
 
-python_jobs = results.find_all(
-    "h2", string=lambda text: "python" in text.lower()
-)
+# # above code has to be modified because string function is case sensitve
 
-python_job_elements = [
-    h2_element.parent.parent.parent for h2_element in python_jobs
-]
+# python_jobs = results.find_all(
+#     "h2", string=lambda text: "python" in text.lower()
+# )
+
+# python_job_elements = [
+#     h2_element.parent.parent.parent for h2_element in python_jobs
+# ]
 
